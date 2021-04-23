90b98080d38a54d2986a0d8fae5d1b7b1b4c4d36
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-04-23 11:05:24 -0300

    pequena correcao

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 
 
 // struct para cada campo
-typedef struct{
+typedef struct {
   int bomb;           // recebe 0 (false) ou 1 (true)
   int open;           // recebe 0 (false) ou 1 (true)
   int adjacentBombs;  // recebe um numero entre 0 e 9
-}field;
+} field;
 
 #define length 10
 // criacao do tabuleiro (matriz[length][length])
