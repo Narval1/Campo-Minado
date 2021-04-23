34ec9e1429e2bd3f9b870ba540985fe0f5bcc72a
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-04-23 11:01:25 -0300

    estrutura base

---
+++ README.md
@@ -0,0 +1,3 @@
+### Campo minado
+
+Projeto 2 da matéria Programação Estruturada - Universidade Federal do ABC
---
+++ main.c
@@ -0,0 +1,23 @@
+// bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+// prototipos de funcoes
+
+
+// struct para cada campo
+typedef struct{
+  int bomb;           // recebe 0 (false) ou 1 (true)
+  int open;           // recebe 0 (false) ou 1 (true)
+  int adjacentBombs;  // recebe um numero entre 0 e 9
+}field;
+
+#define length 10
+// criacao do tabuleiro (matriz[length][length])
+field board[length][length];
+
+int main(void) {
+  
+
+  return 0;
+}
