965a3f06a7a97cf641855b0b7a56eb8897dd917c
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-04-26 18:39:09 -0300

    flag added

--- main.c
+++ main.c
@@ -10,7 +10,8 @@ void savePlayerInfo(char *points);
 typedef struct {
   int bomb;           // recebe 0 (false) ou 1 (true)
   int open;           // recebe 0 (false) ou 1 (true)
-  int adjacentBombs;  // recebe um numero entre 0 e 9
+  int flag;           // recebe 0 (false) ou 1 (true)
+  int adjacentBombs;  // recebe um numero entre 0 e 8
 } field;
 
 #define length 10
