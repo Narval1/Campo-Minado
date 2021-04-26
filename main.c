527eba3160670265de1b3700f7d5fd137a5a6328
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-04-26 13:31:16 -0300

    campo flag adicionado ao struct

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 typedef struct {
   int bomb;           // recebe 0 (false) ou 1 (true)
   int open;           // recebe 0 (false) ou 1 (true)
+  int flag;           // recebe 0 (false) ou 1 (true)
   int adjacentBombs;  // recebe um numero entre 0 e 9
 } field;
 
